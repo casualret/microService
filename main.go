8d94f54a38cc5b41b6842ba2dd1966b6d4a6085c
thsiu <guest@mi-c6.msk.21-school.ru>
2024-09-08 21:37:52 +0300

    add reading cfg, connect to database

--- main.go
+++ main.go
@@ -1,25 +1,95 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
 )
 
-func init() {
-	//config cleanenv
-	//db	pq
-}
-
 func main() {
 
+	//load config
+
+	cfg, err := InitConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	//setup logger
+
+	//connect database && cash
+	db, err := MustNewStorage(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	//init service
+	_ = db
+
+	//init handlers && routs
 	r := InitRoutes()
-	err := r.Run("localhost:8084")
+
+	//start server
+	err = r.Run("localhost:8081")
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
 
+type Config struct {
+	HTTPServer struct {
+		Address     string `mapstructure:"address"`
+		Timeout     string `mapstructure:"timeout"`
+		IdleTimeout string `mapstructure:"idle_timeout"`
+	} `mapstructure:"http_server"`
+	PostgresCfg struct {
+		PgHost     string `mapstructure:"pg_host"`
+		PgPort     string `mapstructure:"pg_port"`
+		PgUser     string `mapstructure:"pg_user"`
+		PgPassword string `mapstructure:"pg_password"`
+		PgDatabase string `mapstructure:"pg_database"`
+		PgSslmode  string `mapstructure:"pg_sslmode"`
+	} `mapstructure:"postgres"`
+}
+
+func InitConfig() (*Config, error) {
+	viper.SetConfigName("config") // config name
+	viper.SetConfigType("yaml")   //config type
+	viper.AddConfigPath("config") // путь к директории конфигурационного файла
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %v", err)
+	}
+
+	var cfg Config
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode config file: %v", err)
+	}
+	return &cfg, nil
+}
+
+type Postgres struct {
+	db *sqlx.DB
+}
+
+func MustNewStorage(cfg *Config) (*Postgres, error) {
+	connInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.PostgresCfg.PgHost, cfg.PostgresCfg.PgPort, cfg.PostgresCfg.PgUser, cfg.PostgresCfg.PgPassword, cfg.PostgresCfg.PgDatabase, cfg.PostgresCfg.PgSslmode)
+	db, err := sqlx.Connect("postgres", connInfo) // connect to postgres
+	if err != nil {
+		return nil, fmt.Errorf("postgres connect error: %v", err)
+	}
+	return &Postgres{db: db}, nil
+}
+
 func InitRoutes() *gin.Engine {
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Пока я мастерил фрегат мир стал бессмыслено богат и полон гнуси")
