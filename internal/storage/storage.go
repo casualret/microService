bc633d27deebe981aa1362a764a6592f301d2695
casualret <nikita.popov.1242@gmail.com>
2024-09-09 20:46:25 +0300

    i dont know

---
+++ internal/storage/storage.go
@@ -0,0 +1,22 @@
+package storage
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"urlshortener/internal/config"
+)
+
+type Postgres struct {
+	db *sqlx.DB
+}
+
+func MustNewStorage(cfg *config.Config) (*Postgres, error) {
+	connInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.PostgresCfg.PgHost, cfg.PostgresCfg.PgPort, cfg.PostgresCfg.PgUser, cfg.PostgresCfg.PgPassword, cfg.PostgresCfg.PgDatabase, cfg.PostgresCfg.PgSslmode)
+	db, err := sqlx.Connect("postgres", connInfo) // connect to postgres
+	if err != nil {
+		return nil, fmt.Errorf("postgres connect error: %v", err)
+	}
+	return &Postgres{db: db}, nil
+}
--- main.go
+++ main.go
@@ -1,66 +1,77 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"github.com/gin-gonic/gin"
-	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
+	"log/slog"
+	"os"
 	"urlshortener/internal/config"
+	"urlshortener/internal/storage"
 )
 
 func main() {
+
+	//setup logger
+
+	var logger *slog.Logger
+
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 
 	//load config
 
 	cfg, err := config.InitConfig()
 	if err != nil {
+		logger.Error("error reading config", err)
 		panic(err)
 	}
 
-	//setup logger
-
 	//connect database && cash
-	db, err := MustNewStorage(cfg)
-	if err != nil { //df
+
+	db, err := storage.MustNewStorage(cfg)
+	if err != nil {
+		logger.Error("error setup storage", err)
 		panic(err)
 	}
 
 	//init service
-	_ = db
+
+	service, err := NewService(db, logger)
+	_ = service
 
 	//init handlers && routs
-	r := InitRoutes()
+
+	handlers := InitRoutes()
 
 	//start server
-	err = r.Run("localhost:8081")
+
+	err = handlers.Run("localhost:8081")
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
 
-type Postgres struct {
-	db *sqlx.DB
+type Operation interface {
+	GetBanners(ctx context.Context, src UserBanner) (*models.BannerWithDetails, error)
 }
 
-func MustNewStorage(cfg *config.Config) (*Postgres, error) {
-	connInfo := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.PostgresCfg.PgHost, cfg.PostgresCfg.PgPort, cfg.PostgresCfg.PgUser, cfg.PostgresCfg.PgPassword, cfg.PostgresCfg.PgDatabase, cfg.PostgresCfg.PgSslmode)
-	db, err := sqlx.Connect("postgres", connInfo) // connect to postgres
-	if err != nil {
-		return nil, fmt.Errorf("postgres connect error: %v", err)
-	}
-	return &Postgres{db: db}, nil
+type Service struct {
+	Operations
+}
+
+func NewService(db *storage.Postgres, logger *slog.Logger) (*Service, error) {
+	return &Service{Operations: NewBannerMana}, nil
 }
 
 func InitRoutes() *gin.Engine {
 
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	//r := gin.Default()
+	r := gin.New()
+
+	r.GET("/user_banner", func(c *gin.Context) {
 		c.String(200, "Пока я мастерил фрегат мир стал бессмыслено богат и полон гнуси")
 	})
-	//r.POST("/create_url", CreateShortUrl(c))
-	//r.GET("/get_url", RedirectUrl(c))
+
 	return r
 }
